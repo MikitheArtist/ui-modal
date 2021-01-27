fe34bf91a298ac68035b65a1d6289855acbd792e
Artem Verbat <artem.verbat@gmail.com>
2021-01-27 06:55:25 +0200

    chore: Add script for build lib. Replace double quotes to single.

--- gulpfile.js
+++ gulpfile.js
@@ -1,80 +1,116 @@
-const gulp = require("gulp");
-const sass = require("gulp-sass");
-const sourcemaps = require("gulp-sourcemaps");
-const gulpIf = require("gulp-if");
-const del = require("del");
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const gulpIf = require('gulp-if');
+const del = require('del');
 const browserSync = require('browser-sync').create();
-const babelify = require('babelify');
 const browserify = require('browserify');
+const babelify = require('babelify');
+const babel = require('gulp-babel');
 const source = require('vinyl-source-stream');
 const buffer = require('vinyl-buffer');
 const uglify = require('gulp-uglify');
 const gutil = require('gulp-util');
 const rename = require('gulp-rename');
 
-const isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV == "development";
+const isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV == 'development';
 
-gulp.task("styles", function (){
-  return gulp.src("src/app.scss")
+gulp.task('views', function () {
+  return gulp.src('./src/**/*.html')
+    .pipe(gulp.dest('./public'));
+});
+
+gulp.task('styles', function () {
+  return gulp.src('./src/app.scss')
     .pipe(gulpIf(isDevelopment, sourcemaps.init()))
     .pipe(sass())
     .pipe(rename('style.css'))
     .pipe(gulpIf(isDevelopment, sourcemaps.write()))
-    .pipe(gulp.dest("dist/styles"));
+    .pipe(gulp.dest('./public/styles'));
 });
 
-gulp.task("clean", function(){
-  return del("dist");
+gulp.task('styles:lib', function () {
+  return gulp.src('./src/app.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(rename('style.css'))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./lib/styles'));
 });
 
-gulp.task("scripts", function() {
+gulp.task('scss:lib', function () {
+  return gulp.src('./src/**/*.scss')
+    .pipe(gulp.dest('./lib/styles'));
+});
+
+gulp.task('scripts', function () {
   return browserify({
-    entries: "./src/app.js",
-    debug: isDevelopment
+    entries: './src/app.js',
+    debug: true
   })
-    .transform(babelify, { presets: [ '@babel/preset-env' ], plugins: [ '@babel/plugin-proposal-class-properties'] })
+    .transform(babelify)
     .bundle()
-    .on("error", function (error) {
-      gutil.log(gutil.colors.red("Error: " + error), "\n", error.codeFrame);
-      this.emit("end");
+    .on('error', function (error) {
+      gutil.log(gutil.colors.red('Error: ' + error), '\n', error.codeFrame);
+      this.emit('end');
     })
-    .pipe(source("bundle.js"))
+    .pipe(source('bundle.js'))
     .pipe(buffer())
     .pipe(gulpIf(!isDevelopment, uglify()))
-    .pipe(gulp.dest("./dist/scripts"));
+    .pipe(gulp.dest('./public/scripts'));
 });
 
-gulp.task("views", function() {
-  return gulp.src("src/**/*.html")
-     .pipe(gulp.dest("dist"));
-
+gulp.task('scripts:lib', function () {
+  return gulp.src('./src/**/*.js')
+    .pipe(babel())
+    .pipe(gulp.dest('./lib/scripts'));
 });
 
-gulp.task("build", gulp.series(
-  "clean", 
-   gulp.parallel("styles", "scripts", "views")
+gulp.task('clean', function () {
+  return del('./public');
+});
+
+gulp.task('clean:lib', function () {
+  return del('./lib');
+});
+
+gulp.task('build', gulp.series(
+  'clean',
+   gulp.parallel(
+     'styles',
+     'scripts',
+     'views'
+   )
 ));
 
-gulp.task("watch", function () {
-  gulp.watch("./src/**/*.html", gulp.series("views"));
-  gulp.watch("./src/**/*.{css,scss}", gulp.series("styles"));
-  gulp.watch("./src/**/*.js", gulp.series("scripts"));
+gulp.task('build:lib', gulp.series(
+  'clean:lib',
+   gulp.parallel(
+     'styles:lib',
+     'scss:lib',
+     'scripts:lib'
+   )
+));
+
+gulp.task('watch', function () {
+  gulp.watch('./src/**/*.html', gulp.series('views'));
+  gulp.watch('./src/**/*.{css,scss}', gulp.series('styles'));
+  gulp.watch('./src/**/*.js', gulp.series('scripts'));
 });
 
 gulp.task('serve', function () {
   browserSync.init({
-    server: './dist',
+    server: './public',
     port: 8080
   });
 
   browserSync.watch('./**/*.*').on('change', browserSync.reload);
 });
 
-gulp.task("default", gulp.series(
-  "build",
+gulp.task('default', gulp.series(
+  'build',
   gulp.parallel(
-    "watch",
-    "serve"    
+    'watch',
+    'serve'
   )
 ));
-
---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+export * from './modal';
+export * from './modal-provider';
+export * from './confirmation-modal';
